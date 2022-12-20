298609b89e12a81f825d230d2fa9d81a2bde7732
Frank Moreno <frankmoreno1993@gmail.com>
2022-12-20 18:26:18 -0500

    fix find_median_in_sorted_arrays_by_index

--- leetcode/src/e0004_median_of_two_sorted_arrays.rs
+++ leetcode/src/e0004_median_of_two_sorted_arrays.rs
@@ -2,19 +2,19 @@ struct Solution;
 
 impl Solution {
   pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    if nums1.is_empty() {
+      return find_median_in_sorted_arrays_by_index(&nums2);
+    }
+
+    if nums2.is_empty() {
+      return find_median_in_sorted_arrays_by_index(&nums1);
+    }
+
     find_mean(nums1, nums2)
   }
 }
 
 fn find_mean(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-  if nums1.is_empty() {
-    return find_median_in_sorted_arrays_by_index(&nums2);
-  }
-
-  if nums2.is_empty() {
-    return find_median_in_sorted_arrays_by_index(&nums1);
-  }
-
   let total = nums1.len() + nums2.len();
 
   let mut index1: usize = 0;
@@ -65,10 +65,10 @@ fn find_mean(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
 fn find_median_in_sorted_arrays_by_index(nums: &[i32]) -> f64 {
   let len = nums.len();
   let mid = len >> 1; // = sum / 2
-  if len % 2 == 0 {
+  if len % 2 == 1 {
     nums[mid] as f64
   } else {
-    ((nums[mid] + nums[mid + 1]) as f64) / 2.0
+    ((nums[mid] + nums[mid - 1]) as f64) / 2.0
   }
 }
 
@@ -91,6 +91,28 @@ mod test {
   fn example_2() {
     let nums1 = vec![1, 2];
     let nums2 = vec![3, 4];
+    assert_eq!(
+      Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone()),
+      2.5
+    );
+    assert_eq!(Solution::find_median_sorted_arrays(nums2, nums1), 2.5);
+  }
+
+  #[test]
+  fn one_is_empty_other_has_odd() {
+    let nums1 = vec![1, 2, 3];
+    let nums2 = vec![];
+    assert_eq!(
+      Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone()),
+      2.0
+    );
+    assert_eq!(Solution::find_median_sorted_arrays(nums2, nums1), 2.0);
+  }
+
+  #[test]
+  fn one_is_empty_other_has_even() {
+    let nums1 = vec![1, 2, 3, 4];
+    let nums2 = vec![];
     assert_eq!(
       Solution::find_median_sorted_arrays(nums1.clone(), nums2.clone()),
       2.5
