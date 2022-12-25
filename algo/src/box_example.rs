83e42a95da75b4e23245badc89f473a93ee8c74a
Frank Moreno <frankmoreno1993@gmail.com>
2022-12-24 20:59:29 -0500

    box example: clear linter warnings

--- algo/src/box_example.rs
+++ algo/src/box_example.rs
@@ -9,16 +9,13 @@ impl Drop for BoxNode {
   }
 }
 
-fn drop_example() {
-  let node = BoxNode { val: 3, next: None };
-}
-
 fn circular_box() {
   let mut a = Box::new(BoxNode { val: 1, next: None }); // x
   let mut b = Box::new(BoxNode { val: 2, next: None }); // y
-  let mut c = Box::new(BoxNode { val: 3, next: None }); // z
+  let c = Box::new(BoxNode { val: 3, next: None }); // z
 
   b.next = Some(c);
-  //a.next = Some(b);
+  a.next = Some(b);
   //a.next.as_mut().unwrap().next.as_mut().unwrap().next = Some(a);
+  drop(a)
 }
